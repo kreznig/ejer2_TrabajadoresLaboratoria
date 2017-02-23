4c18cb82e3134eef87abdce5d708e7e6823f789f
Miriam Peralta <miiriampc@gmail.com>
2017-02-23 12:55:41 -0500

    Primer commit Ejer2 Trabajadores Laboratoria

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+var arrayObj=[ { nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleanios : "23 de enero" },
+ { nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre" },
+ { nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo" },
+ { nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleanios : "10 de octubre" },
+ { nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleanios : "23 de enero" } ];
+
+var arr=arrayObj.forEach(function(e){
+  document.getElementById('ficha').innerHTML+= "<ul><li>Nombre: "+e.nombre+"</li><li>Apellidos: "+
+                                                e.apellido+"</li><li>Rol: "+e.rol+"</li><li>Cumpleaños: "+
+                                                e.cumpleanios+"</li></ul>";
+});
